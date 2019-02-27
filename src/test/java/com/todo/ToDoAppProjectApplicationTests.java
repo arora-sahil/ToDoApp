4d3f18eb7a45168a2db1e7363c43bcbaa8467ee6
Sahil Arora <sahilarora358@gmail.com>
2019-02-28 00:48:53 +0530

    Added Unit Tests for API

--- src/test/java/com/todo/ToDoAppProjectApplicationTests.java
+++ src/test/java/com/todo/ToDoAppProjectApplicationTests.java
@@ -8,6 +8,7 @@ import com.todo.dto.ToDo;
 import com.todo.repository.IToDoRepository;
 import com.todo.repository.ToDoEntity;
 import com.todo.service.ToDoServiceController;
+import org.json.JSONArray;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -21,7 +22,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import java.util.Optional;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.*;
+
 import static org.mockito.Mockito.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
@@ -57,15 +61,40 @@ public class ToDoAppProjectApplicationTests {
 				return toDoEntity;
 			}
 		});
+		ToDoEntity toDoEntity1 = new ToDoEntity();
+		toDoEntity1.setTaskId(1);
+		toDoEntity1.setTaskName("Open Bank Account");
+		toDoEntity1.setTaskPriority("Low");
+		toDoEntity1.setTaskStatus("Not Completed");
+
+		List<ToDoEntity> list = new ArrayList<ToDoEntity>();
+		list.add(toDoEntity);
+		list.add(toDoEntity1);
+		when(iToDoRepository.findAll()).thenReturn(list);
+
 	}
 	@Test
-	public void retrieveTask() throws Exception{
+	public void retrieveAllTasks() throws Exception{
+
+		MvcResult mvcResult = this.mockMvc.perform(get("/tasks")).andExpect(status().isOk()).andReturn();
+		String actual = mvcResult.getResponse().getContentAsString();
+		JSONArray jsonArray = new JSONArray(actual);
+		Assert.assertTrue(jsonArray.length()==2);
+	}
+	@Test
+	public void retrieveTaskByTaskId() throws Exception{
 		MvcResult mvcResult = this.mockMvc.perform(get("/tasks/1")).andExpect(status().isOk()).andReturn();
 		String actual = mvcResult.getResponse().getContentAsString();
 		Assert.assertEquals(Integer.toUnsignedLong(JsonPath.read(actual,"$.taskId")),1);
 		Assert.assertEquals(JsonPath.read(actual,"$.taskName"),"Withdraw Cash");
 		Assert.assertEquals(JsonPath.read(actual,"$.taskPriority"),"High");
 		Assert.assertEquals(JsonPath.read(actual,"$.taskStatus"),"Not Completed");
+	}
+	@Test
+	public void deleteTask() throws Exception{
+		MvcResult mvcResult = this.mockMvc.perform(delete("/tasks/1")).andExpect(status().isOk()).andReturn();
+		String actual = mvcResult.getResponse().getContentAsString();
+		Assert.assertTrue(actual.isEmpty());
 	}
 
 	@Test
@@ -87,6 +116,27 @@ public class ToDoAppProjectApplicationTests {
 		Assert.assertEquals(JsonPath.read(actual,"$.taskName"),"Withdraw Cash");
 		Assert.assertEquals(JsonPath.read(actual,"$.taskPriority"),"High");
 		Assert.assertEquals(JsonPath.read(actual,"$.taskStatus"),"Not Completed");
+	}
+
+	@Test
+	public void updateTask() throws Exception {
+		ToDo toDo = new ToDo();
+		toDo.setTaskId(1);
+		toDo.setTaskName("Opem Bank Account");
+		toDo.setTaskPriority("High");
+		toDo.setTaskStatus("Completed");
+
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+		ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+		String requestJson=ow.writeValueAsString(toDo);
+
+		MvcResult mvcResult = this.mockMvc.perform(put("/tasks/1").accept(APPLICATION_JSON).contentType(APPLICATION_JSON_UTF8).content(requestJson)).andExpect(status().isOk()).andReturn();
+		String actual = mvcResult.getResponse().getContentAsString();
+		Assert.assertEquals(Integer.toUnsignedLong(JsonPath.read(actual,"$.taskId")),1);
+		Assert.assertEquals(JsonPath.read(actual,"$.taskName"),"Opem Bank Account");
+		Assert.assertEquals(JsonPath.read(actual,"$.taskPriority"),"High");
+		Assert.assertEquals(JsonPath.read(actual,"$.taskStatus"),"Completed");
 	}
 
 }
